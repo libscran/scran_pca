be28deffaa1bc9a36f79e1368be94190260c9583
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2026-01-15 08:56:31 +0000

    Deploying to gh-pages from @ libscran/scran_pca@b1ba6295387cea71160cf729824f4fff577d85cf 🚀

--- docs/search/classes_c.js
+++ docs/search/classes_c.js
@@ -1,17 +1,10 @@
 var searchData=
 [
-  ['scaledadjointworkspace_0',['ScaledAdjointWorkspace',['https://ltla.github.io/CppIrlba/classirlba_1_1ScaledAdjointWorkspace.html',1,'irlba']]],
-  ['scaledmatrix_1',['ScaledMatrix',['https://ltla.github.io/CppIrlba/classirlba_1_1ScaledMatrix.html',1,'irlba']]],
-  ['scaledrealizeworkspace_2',['ScaledRealizeWorkspace',['https://ltla.github.io/CppIrlba/classirlba_1_1ScaledRealizeWorkspace.html',1,'irlba']]],
-  ['scaledworkspace_3',['ScaledWorkspace',['https://ltla.github.io/CppIrlba/classirlba_1_1ScaledWorkspace.html',1,'irlba']]],
-  ['simpleadjointworkspace_4',['SimpleAdjointWorkspace',['https://ltla.github.io/CppIrlba/classirlba_1_1SimpleAdjointWorkspace.html',1,'irlba']]],
-  ['simplematrix_5',['SimpleMatrix',['https://ltla.github.io/CppIrlba/classirlba_1_1SimpleMatrix.html',1,'irlba']]],
-  ['simplepcaoptions_6',['SimplePcaOptions',['../structscran__pca_1_1SimplePcaOptions.html',1,'scran_pca']]],
-  ['simplepcaresults_7',['SimplePcaResults',['../structscran__pca_1_1SimplePcaResults.html',1,'scran_pca']]],
-  ['simplerealizeworkspace_8',['SimpleRealizeWorkspace',['https://ltla.github.io/CppIrlba/classirlba_1_1SimpleRealizeWorkspace.html',1,'irlba']]],
-  ['simpleworkspace_9',['SimpleWorkspace',['https://ltla.github.io/CppIrlba/classirlba_1_1SimpleWorkspace.html',1,'irlba']]],
-  ['singlequantile_10',['SingleQuantile',['https://libscran.github.io/scran_blocks/classscran__blocks_1_1SingleQuantile.html',1,'scran_blocks']]],
-  ['singlequantilevariable_11',['SingleQuantileVariable',['https://libscran.github.io/scran_blocks/classscran__blocks_1_1SingleQuantileVariable.html',1,'scran_blocks']]],
-  ['somenumericarray_12',['SomeNumericArray',['https://tatami-inc.github.io/tatami/classtatami_1_1SomeNumericArray.html',1,'tatami']]],
-  ['sparserange_13',['SparseRange',['https://tatami-inc.github.io/tatami/structtatami_1_1SparseRange.html',1,'tatami']]]
+  ['simplematrix_0',['SimpleMatrix',['https://ltla.github.io/CppIrlba/classirlba_1_1SimpleMatrix.html',1,'irlba']]],
+  ['simplepcaoptions_1',['SimplePcaOptions',['../structscran__pca_1_1SimplePcaOptions.html',1,'scran_pca']]],
+  ['simplepcaresults_2',['SimplePcaResults',['../structscran__pca_1_1SimplePcaResults.html',1,'scran_pca']]],
+  ['singlequantile_3',['SingleQuantile',['https://libscran.github.io/scran_blocks/classscran__blocks_1_1SingleQuantile.html',1,'scran_blocks']]],
+  ['singlequantilevariable_4',['SingleQuantileVariable',['https://libscran.github.io/scran_blocks/classscran__blocks_1_1SingleQuantileVariable.html',1,'scran_blocks']]],
+  ['somenumericarray_5',['SomeNumericArray',['https://tatami-inc.github.io/tatami/classtatami_1_1SomeNumericArray.html',1,'tatami']]],
+  ['sparserange_6',['SparseRange',['https://tatami-inc.github.io/tatami/structtatami_1_1SparseRange.html',1,'tatami']]]
 ];
--- docs/search/files_7.js
+++ docs/search/files_7.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['interface_2ehpp_0',['interface.hpp',['https://ltla.github.io/CppIrlba/interface_8hpp.html',1,'']]],
-  ['irlba_2ehpp_1',['irlba.hpp',['https://ltla.github.io/CppIrlba/irlba_8hpp.html',1,'']]]
+  ['index_5fto_5fcontainer_2ehpp_0',['Index_to_container.hpp',['https://tatami-inc.github.io/tatami/Index__to__container_8hpp.html',1,'']]],
+  ['interface_2ehpp_1',['interface.hpp',['https://ltla.github.io/CppIrlba/interface_8hpp.html',1,'']]]
 ];
